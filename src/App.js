416d9d09197305ea0c3ecc93277196fab911fc14
brucetran <tri.tran@tpptechnology.com>
2020-03-20 02:34:02 +0700

    Update code

--- src/App.js
+++ src/App.js
@@ -1,50 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.scss';
-import colorUtils from './commons/colorUtils';
-import { map, filter, get } from 'lodash'
 import Board from './components/Board';
-import CellBoundary from './components/CellBoundary';
-import Cell from './components/Cell';
-
-let draggedItem;
-const colorList = colorUtils.list();
 
 const App = () => {
-
-  const [colors, setColors] = useState(colorList);
-
-  const onDragStart = (colorItem) => {
-    draggedItem = colorItem
-  };
-
-  const onDragOver = index => {
-    const draggedOverItem = get(colors, index);
-    if (draggedItem === draggedOverItem) {
-      return;
-    }
-    let _items = filter(colors, item => item !== draggedItem);
-    _items.splice(index, 0, draggedItem);
-    setColors(_items);
-  };
-
-  const onDragEnd = () => {
-    draggedItem = null;
-  }
 
   return (
     <div className="App">
       <main>
-        <Board>
-          {map(colors, (color, index) => {
-            const cellBoundaryProps = { key: index, index, onDragOver },
-            cellProps = { color, onDragStart, onDragEnd }
-            return (
-              <CellBoundary {...cellBoundaryProps}>
-                <Cell {...cellProps} />
-              </CellBoundary>
-            )
-          })}
-        </Board>
+        <Board />
       </main>
     </div>
   );
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import colorUtils from '../commons/colorUtils';
+import { map, filter, get } from 'lodash';
+import CellBoundary from './CellBoundary';
+import Cell from './Cell';
 
-const Board = (props) => {
+let draggedItem;
+
+const Board = () => {
+
+  const [colors, setColors] = useState(colorUtils.list());
+
+  const onDragStart = (colorItem) => {
+    draggedItem = colorItem
+  };
+
+  const onDragOver = index => {
+    const draggedOverItem = get(colors, index);
+    if (draggedItem === draggedOverItem) {
+      return;
+    }
+    let _items = filter(colors, item => item !== draggedItem);
+    _items.splice(index, 0, draggedItem);
+    setColors(_items);
+  };
+
+  const onDragEnd = () => {
+    draggedItem = null;
+  }
+
   return (
     <ul>
-      { props.children }
+      {map(colors, (color, index) => {
+        const cellBoundaryProps = { key: index, index, onDragOver },
+        cellProps = { color, onDragStart, onDragEnd }
+        return (
+          <CellBoundary {...cellBoundaryProps}>
+            <Cell {...cellProps} />
+          </CellBoundary>
+        )
+      })}
     </ul>
   )
 }
